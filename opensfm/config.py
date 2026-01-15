60ea39a41d689ad7aea9b4e0857b1f63460a3b26
Piero Toffanin <pt@masseranolabs.com>
2026-01-15 15:26:00 -0500

    Double gcp global weight

--- opensfm/config.py
+++ opensfm/config.py
@@ -243,7 +243,7 @@ class OpenSfMConfig:
     # The default vertical standard deviation of the GCPs (in meters)
     gcp_vertical_sd: float = 0.1
     # Global weight for GCPs, expressed a ratio of the sum of (# projections) + (# shots) + (# relative motions)
-    gcp_global_weight: float = 0.02
+    gcp_global_weight: float = 0.04
     # The standard deviation of the rig translation
     rig_translation_sd: float = 0.1
     # The standard deviation of the rig rotation
